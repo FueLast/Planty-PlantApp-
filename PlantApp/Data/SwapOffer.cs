9dc1ecf1d4eef27e38fbd46acaa58b6963b2c866
Gleb <gleb.melnikow@yandex.ru>
2026-04-12 21:10:36 +0300

    feat(models): create new models for plant swap

---
+++ PlantApp/Data/SwapOffer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantApp.Data
+{
+    public class SwapOffer
+    {
+        public int Id { get; set; }
+
+        public int OwnerId { get; set; }
+        public int UserPlantId { get; set; }
+
+        public string? DesiredPlantDescription { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // навигация
+        public User? Owner { get; set; }
+        public UserPlant? Plant { get; set; }
+    }
+}
